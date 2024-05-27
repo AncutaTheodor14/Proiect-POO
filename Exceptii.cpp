40fc8169d287c5f1f707c986c99900dbc4e8d110
Theo <ancutatheodor17@gmail.com>
2024-05-27 23:29:17 +0300

    Tema 2.4

--- Exceptii.cpp
+++ Exceptii.cpp
@@ -2,16 +2,17 @@
 // Created by Theo on 5/27/2024.
 //
 
+#include<string>
 #include "Exceptii.h"
 
 eroare_echipa::eroare_echipa(const std::string &Mesaj) : eroare_aplicatie(
-        std::string("Eroare la nivelul echipei, ") + Mesaj) {}
+        "Eroare la nivelul echipei, " + Mesaj) {}
 
 eroare_transfer::eroare_transfer(const std::string &Nume) : eroare_echipa(
-        std::string("membrul echipei ") + Nume + std::string(" nu se poate transfera")) {}
+        "membrul echipei " + Nume + " nu se poate transfera") {}
 
 eroare_campionat::eroare_campionat(const std::string &Mesaj) : eroare_aplicatie(
-        std::string("Eroare la nivelul campionatului, ") + Mesaj) {}
+        "Eroare la nivelul campionatului, " + Mesaj) {}
 
 eroare_numar_jucatori::eroare_numar_jucatori(int numar) : eroare_campionat(
-        std::string("echipa are in componenta ") + std::to_string(numar) + std::string(" membrii")) {}
+        "echipa are in componenta " + std::to_string(numar) + " membrii") {}
